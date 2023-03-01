bdfb5cb8ce869f9cb7a3e155adefc9aae3e02437
mmorrii <byryshka55@gmail.com>
2023-03-01 12:18:26 +0200

    add new block

--- src/js/block4.js
+++ src/js/block4.js
@@ -1,23 +1,4 @@
 $(document).ready(function () {
-   //$('.block4__slider').slick({
-   //   infinite: false,
-   //   arrows: false,
-   //   dots: true,
-   //   appendDots: '.block4__slider-dots',
-   //   swipeToSlide: true,
-   //   mobileFirst: true,
-   //   variableWidth: true,
-   //   centerMode: true,
-   //   //responsive: [
-   //   //   {
-   //   //      breakpoint: 321,
-   //   //      settings: {
-   //   //         dots: false,
-   //   //      }
-   //   //   }
-   //   //]
-   //});
-
    if ($(window).width() <= 630) {
       $('.block4__slider').slick({
          infinite: false,
@@ -30,14 +11,4 @@ $(document).ready(function () {
    } else {
       $('.block4__slider').slick('unslick');
    }
-
-
-   //---ADD IMAGE IF WINDOW WIDTH MORE THEN 540PX----------------------------------------------------------------------------------
-   //window.addEventListener('resize', function () {
-   //   if (window.innerWidth >= 630) {
-   //      $('#block1').after('<img class="arrow-up" src="../img/arrow-up.svg" alt="">');
-   //   } else {
-   //   }
-   //});
-
 });
