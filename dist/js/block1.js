3b24a2ec703e866c01da8c39ed7d27ca6c9a1d3c
German Trushkov <germantrushkov@gmail.com>
2023-03-02 19:44:24 +0200

    delete dist

--- dist/js/block1.js
+++
@@ -1,9 +0,0 @@
-let myImg = document.querySelector('#myImg')
-
-window.addEventListener('resize', function () {
-   if (window.innerWidth >= 900) {
-      myImg.src = '../img/planet-1440.svg';
-   } else {
-      myImg.src = '../img/planet.svg';
-   }
-});
