0b6e6174b430c66dd493d875d548841438988c7a
mmorrii <byryshka55@gmail.com>
2023-03-03 11:07:27 +0200

    accept new dungeon

--- dist/js/block9.js
+++ dist/js/block9.js
@@ -1,18 +1,24 @@
-//$(document).ready(function () {
-//   if ($(window).width() <= 1200) {
-//      $('.block9__slider-top').slick({
-//         arrows: false,
-//         swipe: false,
-//         fade: true,
-//      });
+$(document).ready(function () {
+   if ($(window).width() <= 1200) {
+      $('.block9__slider-top').slick({
+         arrows: false,
+         swipe: false,
+         fade: true,
+         asNavFor: '.block9__slider-bottom',
+         adaptiveHeight: true,
+         infinite: false,
+      });
 
-//      $('.block9__slider-bottom').slick({
-//         arrows: false,
-//         dots: true,
-//         infinite: false,
-//         appendDots: '.block9__slider-dots',
-//      });
-//   } else {
-//      $('.block7__row').slick('unslick');
-//   }
-//});
+      $('.block9__slider-bottom').slick({
+         arrows: false,
+         dots: true,
+         infinite: false,
+         appendDots: '.block9__slider-dots',
+         asNavFor: '.block9__slider-top',
+         adaptiveHeight: true,
+      });
+   } else {
+      $('.block9__slider-top').slick('unslick');
+      $('.block9__slider-bottom').slick('unslick');
+   }
+});
